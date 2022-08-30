fdfb2f58510261e96423842c91ea825c3f203fef
toto <50374594+toine08@users.noreply.github.com>
2022-08-30 22:32:32 +0200

    npm install && install web3js

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,18 @@ import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
 import * as Web3 from '@solana/web3.js'
-
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
 
   const addressSubmittedHandler = (address: string) => {
-    try {
-      setAddress(address)
-      const key = new Web3.PublicKey(address)
-      const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
-      connection.getBalance(key).then(balance => {
-        setBalance(balance / Web3.LAMPORTS_PER_SOL)
-      })
-    } catch (error) {
-      setAddress('')
-      setBalance(0)
-      alert(error)
-    }
+    const key = new Web3.PublicKey(address)  
+    setAddress(key.toBase58())
+    const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+    connection.getBalance(key).then(balance =>{
+      setBalance(balance / Web3.LAMPORTS_PER_SOL)
+    })
+      setBalance(1000)
   }
 
   return (
